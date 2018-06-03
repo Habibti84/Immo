4ca146d57be1d29897e2177716b05ba98f9a704c
Jacqueline Motzer <jacmotzer@gmail.com>
2018-06-03 10:19:08 +0200

    03.06.18

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Sacramento" rel="stylesheet">
+    <title>Immo</title>
+  </head>
+  <body>
+    <div id="logo">
+      <h1>Finden Sie Ihr neues Zuhause</h1>
+
+    </div>
+
+    <div id="nav">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Objekte</a></li>
+        <li><a href="#">Kontakt</a></li>
+      </ul>
+
+    </div>
+
+    <div id="header">
+
+    </div>
+
+  </body>
+</html>
